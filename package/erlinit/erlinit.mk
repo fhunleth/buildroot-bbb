bd9612e47cf470b4c63acff0edc2f64d7de55deb
Frank Hunleth <fhunleth@troodon-software.com>
2013-08-16 12:04:22 -0400

    new package: erlinit

---
+++ package/erlinit/erlinit.mk
@@ -0,0 +1,24 @@
+################################################################################
+#
+# erlinit
+#
+################################################################################
+
+ERLINIT_VERSION = ac5f3c2
+ERLINIT_SITE = http://github.com/fhunleth/erlinit/tarball/$(ERLINIT_VERSION)
+ERLINIT_LICENSE = MIT 
+
+# Make sure erlinit wins over busybox init
+ifeq ($(BR2_PACKAGE_BUSYBOX),y)
+ERLINIT_DEPENDENCIES += busybox
+endif
+
+define ERLINIT_BUILD_CMDS
+	$(MAKE1) $(TARGET_CONFIGURE_OPTS) -C $(@D)
+endef
+
+define ERLINIT_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 755 $(@D)/erlinit $(TARGET_DIR)/sbin/init
+endef
+
+$(eval $(generic-package))
