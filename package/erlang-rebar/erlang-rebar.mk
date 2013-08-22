46f6d7d497662315e833c173997092f180a2d7c2
Frank Hunleth <fhunleth@troodon-software.com>
2013-08-22 15:26:41 -0400

    erlang-rebar: add env for compiling c code

--- package/erlang-rebar/erlang-rebar.mk
+++ package/erlang-rebar/erlang-rebar.mk
@@ -13,8 +13,15 @@ ERLANG_REBAR_DEPENDENCIES = erlang host-erlang
 HOST_ERLANG_REBAR_DEPENDENCIES = host-erlang
 
 # Macro for invoking rebar in other packages
-REBAR = REBAR_DEPS_DIR=$(ERLANG_PACKAGE_INSTALL_DIR) \
-                $(HOST_DIR)/usr/bin/rebar
+REBAR = $(HOST_MAKE_ENV) \
+	CC="$(TARGET_CC)" \
+	CXX="$(TARGET_CXX)" \
+	CFLAGS="$(TARGET_CFLAGS)" \
+	CXXFLAGS="$(TARGET_CXXFLAGS)" \
+	LDFLAGS="$(TARGET_LDFLAGS)" \
+	ERL_CFLAGS="-I$(STAGING_DIR)/usr/lib/erlang/erts-5.9.3.1/include -I$(STAGING_DIR)/usr/lib/erlang/lib/erl_interface-3.7.9/include" \
+	ERL_LDFLAGS="-L$(STAGING_DIR)/usr/lib/erlang/erts-5.9.3.1/lib -L$(STAGING_DIR)/usr/lib/erlang/lib/erl_interface-3.7.9/lib -lerts -lei" \
+	$(HOST_DIR)/usr/bin/rebar -vvv deps_dir=$(ERLANG_PACKAGE_INSTALL_DIR)
 
 define ERLANG_REBAR_BUILD_CMDS
 	(cd $(@D); $(HOST_MAKE_ENV) ./bootstrap)
