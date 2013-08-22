462593c5e6f583b27661bd360e17ad894c754a21
Frank Hunleth <fhunleth@troodon-software.com>
2013-08-22 15:01:27 -0400

    erlang: install to staging and prune target
    Several header files and libraries are needed to compile
    erlang applications, so make these available in staging.
    Now that they're there, prune out the headers and other
    files that were partially present in the target directory.

--- package/erlang/erlang.mk
+++ package/erlang/erlang.mk
@@ -14,6 +14,7 @@ ERLANG_LICENSE = EPL
 ERLANG_LICENSE_FILES = EPLICENCE
 ERLANG_INSTALL_STAGING = YES
 
+ERLANG_INSTALL_STAGING = YES
 ERLANG_PACKAGE_INSTALL_DIR = $(TARGET_DIR)/usr/lib/erlang/lib
 
 # The configure checks for these functions fail incorrectly
@@ -48,15 +49,25 @@ ifneq ($(BR2_PACKAGE_ERLANG_MEGACO),y)
 ERLANG_REMOVE_PACKAGES += megaco
 endif
 
-define ERLANG_REMOVE_UNUSED
-	find $(TARGET_DIR)/usr/lib/erlang -type d -name src -prune -exec rm -rf {} \;
-	find $(TARGET_DIR)/usr/lib/erlang -type d -name examples -prune -exec rm -rf {} \;
+define ERLANG_REMOVE_TARGET_UNUSED
+	# Miscellaneous files not used on the target
+	rm -rf $(TARGET_DIR)/usr/lib/erlang/misc
+	rm -f $(TARGET_DIR)/usr/lib/erlang/Install
+	rm -rf $(TARGET_DIR)/usr/lib/erlang/usr/include
+
+	# Package and erts files only needed at compile time
+	# or for documentation
+	for dir in $(TARGET_DIR)/usr/lib/erlang/erts-* $(TARGET_DIR)/usr/lib/erlang/lib/*; do \
+		rm -rf $${dir}/src $${dir}/c_src $${dir}/include $${dir}/doc $${dir}/man $${dir}/examples $${dir}/emacs; \
+	done
+
+	# Unneeded packages
 	for package in $(ERLANG_REMOVE_PACKAGES); do \
 		rm -rf $(ERLANG_PACKAGE_INSTALL_DIR)/$${package}-*; \
 	done
 endef
 
-ERLANG_POST_INSTALL_TARGET_HOOKS += ERLANG_REMOVE_UNUSED
+ERLANG_POST_INSTALL_TARGET_HOOKS += ERLANG_REMOVE_TARGET_UNUSED
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
