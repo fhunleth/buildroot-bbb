baca134ac95e936fde2ac47a2539716a678233ff
Frank Hunleth <fhunleth@troodon-software.com>
2013-09-08 14:18:44 -0400

    Majorly trim down Erlang installs
    rootfs is ~12MB

---
+++ board/beagleboneblack/pre-createfs.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#
+# Pre create filesystem hook
+#  - Prune out files that we don't need
+
+TARGETDIR=$1
+
+# All of the Erlang libraries get included from
+# the release, so we don't need anything in here.
+rm -fr $TARGETDIR/usr/lib/erlang/lib/*
+
+# Clean up the release
+find $TARGETDIR/srv/erlang -name "*~" -exec rm "{}" ";"
+find $TARGETDIR/srv/erlang -name "src" -exec rm -fr "{}" ";"
+find $TARGETDIR/srv/erlang -name "include" -exec rm -fr "{}" ";"
+rm -fr $TARGETDIR/srv/erlang/bin
+
