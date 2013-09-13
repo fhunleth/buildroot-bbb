2690fb494b250433cdff8f9f348d5c8c8238492b
Frank Hunleth <fhunleth@troodon-software.com>
2013-09-13 18:43:29 -0400

    Align armerl and bbberl configs; keep erl libs

--- board/beagleboneblack/pre-createfs.sh
+++ board/beagleboneblack/pre-createfs.sh
@@ -8,11 +8,12 @@ TARGETDIR=$1
 
 # All of the Erlang libraries get included from
 # the release, so we don't need anything in here.
-rm -fr $TARGETDIR/usr/lib/erlang/lib/*
+#rm -fr $TARGETDIR/usr/lib/erlang/lib/*
 
 # Clean up the release
 find $TARGETDIR/srv/erlang -name "*~" -exec rm "{}" ";"
 find $TARGETDIR/srv/erlang -name "src" -exec rm -fr "{}" ";"
 find $TARGETDIR/srv/erlang -name "include" -exec rm -fr "{}" ";"
-rm -fr $TARGETDIR/srv/erlang/bin
+find $TARGETDIR/srv/erlang -name "obj" -exec rm -fr "{}" ";"
+rm -fr $TARGETDIR/srv/erlang/bin srv/erlang/erts-*
 
