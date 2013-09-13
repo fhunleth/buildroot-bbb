91a74eda1b0fbff4e4178d520a5dbbaafff21a29
Frank Hunleth <fhunleth@troodon-software.com>
2013-09-13 13:31:10 -0400

    erlang: clean up staging and target
    This removes a lot of files that were being put
    in staging and target. Staging is clean up to
    avoid referencing libraries that won't be available
    on the target and to keep the release tools from
    copying intermediate files around.

--- package/erlang/erlang.mk
+++ package/erlang/erlang.mk
@@ -53,24 +53,41 @@ ifneq ($(BR2_PACKAGE_ERLANG_MEGACO),y)
 ERLANG_REMOVE_PACKAGES += megaco
 endif
 
+define ERLANG_REMOVE_STAGING_UNUSED
+	# Remove intermediate build products from being copied around
+	# by Erlang release tools
+	find $(STAGING_DIR)/usr/lib/erlang -type d -name obj -prune -exec rm -rf {} \;
+
+	# Don't expose packages that won't be available on the target
+	for package in $(ERLANG_REMOVE_PACKAGES); do \
+		rm -rf $(STAGING_DIR)/usr/lib/erlang/lib/$${package}-*; \
+	done
+endef
+
 define ERLANG_REMOVE_TARGET_UNUSED
-	# Miscellaneous files not used on the target
+	# Remove top level installation and header files
 	rm -rf $(TARGET_DIR)/usr/lib/erlang/misc
 	rm -f $(TARGET_DIR)/usr/lib/erlang/Install
 	rm -rf $(TARGET_DIR)/usr/lib/erlang/usr/include
 
-	# Package and erts files only needed at compile time
-	# or for documentation
-	for dir in $(TARGET_DIR)/usr/lib/erlang/erts-* $(TARGET_DIR)/usr/lib/erlang/lib/*; do \
-		rm -rf $${dir}/src $${dir}/c_src $${dir}/include $${dir}/doc $${dir}/man $${dir}/examples $${dir}/emacs; \
+	# Remove intermediate build products
+	find $(TARGET_DIR)/usr/lib/erlang -type d -name obj -prune -exec rm -rf {} \;
+
+	# Remove package and erts files that are not needed
+	# at runtime
+	for dir in $(TARGET_DIR)/usr/lib/erlang/erts-* \
+		   $(TARGET_DIR)/usr/lib/erlang/lib/*; do \
+		rm -rf $${dir}/src $${dir}/c_src $${dir}/include $${dir}/doc \
+		       $${dir}/man $${dir}/examples $${dir}/emacs; \
 	done
 
-	# Unneeded packages
+	# Remove unwanted packages
 	for package in $(ERLANG_REMOVE_PACKAGES); do \
-		rm -rf $(ERLANG_PACKAGE_INSTALL_DIR)/$${package}-*; \
+		rm -rf $(TARGET_DIR)/usr/lib/erlang/lib/$${package}-*; \
 	done
 endef
 
+ERLANG_POST_INSTALL_STAGING_HOOKS += ERLANG_REMOVE_STAGING_UNUSED
 ERLANG_POST_INSTALL_TARGET_HOOKS += ERLANG_REMOVE_TARGET_UNUSED
 
 $(eval $(autotools-package))
