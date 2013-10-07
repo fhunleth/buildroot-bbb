be1b9ab05463626f1ef08546fcfc32ba098125a4
Frank Hunleth <fhunleth@troodon-software.com>
2013-10-07 19:05:33 -0400

    erlang-rebar: make host-only
    This is another tool that doesn't make much sense on
    the target.

--- package/erlang-rebar/erlang-rebar.mk
+++ package/erlang-rebar/erlang-rebar.mk
@@ -4,12 +4,11 @@
 #
 #############################################################
 
-ERLANG_REBAR_VERSION = 620c4b01c6
+ERLANG_REBAR_VERSION = 179ed48
 ERLANG_REBAR_SITE = http://github.com/rebar/rebar/tarball/$(ERLANG_REBAR_VERSION)
 ERLANG_REBAR_LICENSE = Apache-2.0 
 ERLANG_REBAR_LICENSE_FILE = LICENSE 
 
-ERLANG_REBAR_DEPENDENCIES = erlang host-erlang
 HOST_ERLANG_REBAR_DEPENDENCIES = host-erlang
 
 ERLANG_ERTS_DIR = `ls -d $(STAGING_DIR)/usr/lib/erlang/erts-*`
@@ -26,14 +25,6 @@ REBAR = $(HOST_MAKE_ENV) \
 	ERL_LDFLAGS="-L$(ERLANG_ERTS_DIR)/lib -L$(ERLANG_ERL_INTERFACE_DIR)/lib -lerts -lei" \
 	$(HOST_DIR)/usr/bin/rebar -vv
 
-define ERLANG_REBAR_BUILD_CMDS
-	(cd $(@D); $(HOST_MAKE_ENV) ./bootstrap)
-endef
-
-define ERLANG_REBAR_INSTALL_TARGET_CMDS
-	cp $(@D)/rebar $(TARGET_DIR)/usr/bin
-endef
-
 define HOST_ERLANG_REBAR_BUILD_CMDS
 	(cd $(@D); $(HOST_MAKE_ENV) ./bootstrap)
 endef
@@ -42,5 +33,4 @@ define HOST_ERLANG_REBAR_INSTALL_CMDS
 	cp $(@D)/rebar $(HOST_DIR)/usr/bin
 endef
 
-$(eval $(generic-package))
 $(eval $(host-generic-package))
