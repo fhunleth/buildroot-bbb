659d813a2e5c07bdb7a05bb912b2d55f2d9e1146
Frank Hunleth <fhunleth@troodon-software.com>
2013-10-07 18:51:59 -0400

    erlang-relx: pull in upstream fix and simplify
    The upstream fix addressed the need to build in a directory
    called relx instead of any directory name. Also, make this a
    host-only package since it doesn't make sense on target.

--- package/erlang-relx/erlang-relx.mk
+++ package/erlang-relx/erlang-relx.mk
@@ -4,38 +4,23 @@
 #
 #############################################################
 
-ERLANG_RELX_VERSION = 37bd98a
+ERLANG_RELX_VERSION = 73b166d
 ERLANG_RELX_SITE = http://github.com/erlware/relx/tarball/$(ERLANG_RELX_VERSION)
 ERLANG_RELX_LICENSE = Apache-2.0 
 ERLANG_RELX_LICENSE_FILE = LICENSE.md
 
-ERLANG_RELX_DEPENDENCIES = erlang host-erlang host-erlang-rebar
 HOST_ERLANG_RELX_DEPENDENCIES = host-erlang host-erlang-rebar
 
 # Macro for invoking relx in other packages
 RELX = $(HOST_MAKE_ENV) \
 	$(HOST_DIR)/usr/bin/relx
 
-define HOST_ERLANG_RELX_EXTRACT_CMDS
-	mkdir -p $(@D)/relx
-	gzip -d -c $(DL_DIR)/$(ERLANG_RELX_SOURCE) | tar --strip-components=1 -C $(@D)/relx -xf -
-endef
-
-define ERLANG_RELX_BUILD_CMDS
-	$(MAKE) -C $(@D)/relx
-endef
-
-define ERLANG_RELX_INSTALL_TARGET_CMDS
-	cp $(@D)/relx/relx $(TARGET_DIR)/usr/bin
-endef
-
 define HOST_ERLANG_RELX_BUILD_CMDS
-	$(MAKE) -C $(@D)/relx
+	$(MAKE1) -C $(@D)
 endef
 
 define HOST_ERLANG_RELX_INSTALL_CMDS
-	cp $(@D)/relx/relx $(HOST_DIR)/usr/bin
+	cp $(@D)/relx $(HOST_DIR)/usr/bin
 endef
 
-$(eval $(generic-package))
 $(eval $(host-generic-package))
