03039b24f8d944693c71bd50a72f7a1a2dc49598
Frank Hunleth <fhunleth@troodon-software.com>
2013-10-08 16:26:03 -0400

    erlinit: pull in fixed release finding version

--- package/erlinit/erlinit.mk
+++ package/erlinit/erlinit.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ERLINIT_VERSION = 52488bb
+ERLINIT_VERSION = 5a616c7
 ERLINIT_SITE = http://github.com/fhunleth/erlinit/tarball/$(ERLINIT_VERSION)
 ERLINIT_LICENSE = MIT 
 
