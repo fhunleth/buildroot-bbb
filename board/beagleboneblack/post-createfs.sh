473bed9da9d14f637253adb4e2c1d44a52de20d3
Frank Hunleth <fhunleth@troodon-software.com>
2013-10-08 10:17:15 -0400

    Fix up bbb firmware update scripts

--- board/beagleboneblack/post-createfs.sh
+++ board/beagleboneblack/post-createfs.sh
@@ -9,3 +9,8 @@ BOARDDIR=board/beagleboneblack
 IMAGESDIR=$TARGETDIR/../images
 
 $BOARDDIR/am335xpackager.py -c $BOARDDIR/bbb-sdcard.cfg -s $IMAGESDIR/MLO -u $IMAGESDIR/u-boot.img -r $IMAGESDIR/rootfs.ext2 -f $IMAGESDIR/bbb.fw -g $IMAGESDIR/bbb-sdcard.img
+
+# Copy firmware updater script to the images directory
+# for convenience
+cp $BOARDDIR/rootfs-additions/usr/bin/fwupdate $IMAGESDIR
+
--- board/beagleboneblack/pre-createfs.sh
+++ board/beagleboneblack/pre-createfs.sh
@@ -8,13 +8,13 @@ TARGETDIR=$1
 
 # All of the Erlang libraries get included from
 # the release, so we don't need anything in here.
-#rm -fr $TARGETDIR/usr/lib/erlang/lib/*
+rm -fr $TARGETDIR/usr/lib/erlang/lib/*
 
 # Clean up the release
-find $TARGETDIR/srv/erlang -name "*~" -exec rm "{}" ";"
-find $TARGETDIR/srv/erlang -name "src" -exec rm -fr "{}" ";"
-find $TARGETDIR/srv/erlang -name "include" -exec rm -fr "{}" ";"
-find $TARGETDIR/srv/erlang -name "obj" -exec rm -fr "{}" ";"
+find $TARGETDIR/srv/erlang -name "*~" -type f -exec rm "{}" ";"
+find $TARGETDIR/srv/erlang -name "src" -type d -exec rm -fr "{}" ";"
+find $TARGETDIR/srv/erlang -name "include" -type d -exec rm -fr "{}" ";"
+find $TARGETDIR/srv/erlang -name "obj" -type d -exec rm -fr "{}" ";"
 rm -fr $TARGETDIR/srv/erlang/bin srv/erlang/erts-*
 
 # Fix up multilib
